334ae553bef861bf678c1d8472c0473a4342878b
Ángela <angela.lbsr@gmail.com>
2024-10-05 23:32:17 +0200

    rectract tongue while attached

--- godot/src/frog/xsm/frog_attached.gd
+++ godot/src/frog/xsm/frog_attached.gd
@@ -2,13 +2,36 @@
 @icon("res://addons/xsm/icons/state_animation.png")
 extends XSMFrog
 
+var reel_speed: float
+
 var anchor: Vector2
+var max_length: float
+var max_length_squared: float
+
 
 func _on_enter(args) -> void:
 	anchor = args
+	max_length = get_body().global_position.distance_to(anchor)
+	max_length_squared = pow(max_length + 1, 2)
+	
+	reel_speed = get_body().reel_speed
 	
 
-# TODO: retract
+
+func _on_update(delta: float) -> void:
+	if Input.is_action_pressed("left_click"):
+		max_length -= reel_speed * delta
+		max_length_squared = pow(max_length + 1, 2)
+	
+	if get_body().global_position.distance_squared_to(anchor) > max_length_squared:
+		get_body().global_position = anchor + anchor.direction_to(get_body().global_position) * max_length
+	
+	get_body().move_and_slide()
+	
+	if not get_ctl().is_feet_on_ground():
+		change_state("swing", anchor)
+	
+ 
 
 func _on_tongue_detached() -> void:
 	change_state("idle")
--- godot/src/frog/xsm/frog_swing.gd
+++ godot/src/frog/xsm/frog_swing.gd
@@ -2,13 +2,23 @@
 @icon("res://addons/xsm/icons/state_animation.png")
 extends XSMFrog
 
-var gravity:= 1000 * Vector2.DOWN
-var speed:= 5 * Vector2.RIGHT
+var gravity: Vector2
+var speed: Vector2
+var reel_speed: float
 
 var anchor: Vector2
+var max_length: float
+var max_length_squared: float
+
 
 func _on_enter(args) -> void:
 	anchor = args
+	max_length = get_body().global_position.distance_to(anchor)
+	max_length_squared = pow(max_length + 1, 2)
+	
+	gravity = get_body().swing_gravity * Vector2.DOWN
+	speed = get_body().swing_speed * Vector2.RIGHT
+	reel_speed = get_body().reel_speed
 	
 
 func _on_update(delta: float) -> void:
@@ -21,6 +31,14 @@ func _on_update(delta: float) -> void:
 	get_body().velocity = length * tangent
 	get_body().move_and_slide()
 	
+	if Input.is_action_pressed("left_click"):
+		max_length -= reel_speed * delta
+		max_length_squared = pow(max_length + 1, 2)
+		
+	
+	if get_body().global_position.distance_squared_to(anchor) > max_length_squared:
+		get_body().global_position = anchor + anchor.direction_to(get_body().global_position) * max_length
+		 
 	get_ctl().check_direction_change(length)
 	
 	if get_ctl().is_feet_on_ground():
