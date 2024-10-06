1b9e1acd9274acefdbebf7e64baf31dd1702dc2e
erebrus <erebrus@gmail.com>
2024-10-06 14:47:52 +0100

    more sounds

--- godot/src/levels/base_level.gd
+++ godot/src/levels/base_level.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
 @onready var version: Label = %Version
+@onready var ambient: AudioStreamPlayer = $ambient
 
 var debug:=false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	version.text=ProjectSettings.get_setting("application/config/version") # Replace with function body.
 	version.visible=Globals.debug_build
+	ambient.play()
 func _input(event: InputEvent) -> void:
 	if not debug and Input.is_action_just_pressed("debug"):
 		debug = true
 		Events.debug_toggled.emit(debug)
+	
