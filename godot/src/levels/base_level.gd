aa545dd09780c94b16758853a89e121345c80e4f
erebrus <erebrus@gmail.com>
2024-10-06 22:38:09 +0100

    music integration

--- godot/src/levels/base_level.gd
+++ godot/src/levels/base_level.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var ambient: AudioStreamPlayer = $ambient
 @onready var frog: Frog = %Frog
 @onready var health_bar: HealthBar = %HealthBar
+@onready var music: AudioStreamPlayer = $music
 
 var debug:=false
 # Called when the node enters the scene tree for the first time.
@@ -13,7 +14,7 @@ func _ready() -> void:
 	ambient.play()
 	frog.health_component.max_changed.connect(func (x):health_bar.set_max(frog.health_component.max_energy);Logger.info("Changing healthbar"))
 	health_bar.set_max(frog.health_component.max_energy)
-
+	Globals.music_manager.fade_in_stream(music)
 func _input(event: InputEvent) -> void:
 	if not debug and Input.is_action_just_pressed("debug"):
 		debug = true
