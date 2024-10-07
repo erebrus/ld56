05f20ec070e91c937b5c836ae081c18bea8abfb3
erebrus <erebrus@gmail.com>
2024-10-07 10:46:14 +0100

    Update frog_death.gd

--- godot/src/frog/xsm/frog_death.gd
+++ godot/src/frog/xsm/frog_death.gd
@@ -3,9 +3,12 @@
 extends XSMFrog
 class_name XSMDeath
 
+func _on_update(_delta) -> void:
 
+	get_ctl().gravity = get_ctl().apply_falling_gravity(get_ctl().default_gravity)
+	get_ctl().process_movement(_delta)
 func _on_enter(_args) -> void:
 	Logger.debug("state:%s " % name)
-	await get_tree().create_timer(.1).timeout
-	get_body().xsm.disabled=true
+	#await get_tree().create_timer(.1).timeout
+	#get_body().xsm.disabled=true
 	
