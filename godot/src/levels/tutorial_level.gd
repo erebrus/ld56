e5c4f7e9ab4860ed8898ee4a70475b7ce471ab42
erebrus <erebrus@gmail.com>
2024-10-07 20:55:08 +0100

    lose screen changes

--- godot/src/levels/tutorial_level.gd
+++ godot/src/levels/tutorial_level.gd
@@ -2,6 +2,9 @@ extends "res://src/levels/base_level.gd"
 
 @onready var eagle = $Eagle
 
-
+func _ready():
+	super._ready()
+	Events.game_lost.connect(func(x):$Tutorial.visible=false)
+	
 func _on_trigger_eagle_body_entered(body):
 	eagle.trigger_now()
