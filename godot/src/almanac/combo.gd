513f6847ccda739ca9c8c4380f2dfc6aae256f2b
Ángela <angela.lbsr@gmail.com>
2024-10-07 22:05:11 +0200

    fix almanac

--- godot/src/almanac/combo.gd
+++ godot/src/almanac/combo.gd
@@ -15,16 +15,24 @@ extends VBoxContainer
 @export var bug1: Types.BugType:
 	set(value):
 		bug1 = value
-		$Bugs/Bug1.texture = Types.BUG_ALMANAC_TEXTURES[value]
+		if $Bugs/Bug1:
+			$Bugs/Bug1.texture = Types.BUG_ALMANAC_TEXTURES[bug1]
 		
 
 @export var bug2: Types.BugType:
 	set(value):
 		bug2 = value
-		$Bugs/Bug2.texture = Types.BUG_ALMANAC_TEXTURES[value]
+		if $Bugs/Bug2:
+			$Bugs/Bug2.texture = Types.BUG_ALMANAC_TEXTURES[bug2]
 
 
 @export var bug3: Types.BugType:
 	set(value):
 		bug3 = value
-		$Bugs/Bug3.texture = Types.BUG_ALMANAC_TEXTURES[value]
+		if $Bugs/Bug3:
+			$Bugs/Bug3.texture = Types.BUG_ALMANAC_TEXTURES[bug3]
+
+func _ready() -> void:
+	$Bugs/Bug1.texture = Types.BUG_ALMANAC_TEXTURES[bug1]
+	$Bugs/Bug2.texture = Types.BUG_ALMANAC_TEXTURES[bug2]
+	$Bugs/Bug3.texture = Types.BUG_ALMANAC_TEXTURES[bug3]
