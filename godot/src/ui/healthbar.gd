d26af01d53ab51aa448a313c2fa377a355c543de
Ángela <angela.lbsr@gmail.com>
2024-11-01 12:55:19 +0100

    fix healthbar - for real

--- godot/src/ui/healthbar.gd
+++ godot/src/ui/healthbar.gd
@@ -6,15 +6,16 @@ class_name HealthBar
 
 
 func _ready():
-	Events.block_max_hp.connect(func(x):set_block(x))
+	Events.block_max_hp.connect(set_block)
+	grey_progress.value = 0
 
 func _on_frog_energy_changed(value: float) -> void:
-	$Progress.value = value
+	if progress:
+		progress.value = value
 
 func set_max(value:float)->void:
 	progress.max_value=value
 	grey_progress.max_value=value
 	
-func set_block(value:float):	
+func set_block(value:float):
 	grey_progress.value=value
-	grey_progress.visible = value !=0
