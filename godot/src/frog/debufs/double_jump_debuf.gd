6bc6ec19a5dd4b36f1bbc569a985f975163c787f
Ángela <angela.lbsr@gmail.com>
2024-11-18 15:40:03 +0100

    squish animation

--- godot/src/frog/debufs/double_jump_debuf.gd
+++ godot/src/frog/debufs/double_jump_debuf.gd
@@ -5,7 +5,9 @@ extends Debuf
 func apply_debuf(frog: Frog):
 	super.apply_debuf(frog)
 	frog.controller.max_jump_amount-=1
+	frog.debuff_animation_player.play("debuff_animations/squish")
 	
 func cancel_debuf(frog: Frog):
 	super.cancel_debuf(frog)
 	frog.controller.max_jump_amount+=1
+	frog.debuff_animation_player.play_backwards("debuff_animations/squish")
