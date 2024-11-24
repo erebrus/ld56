d82f9b62578c21c75b79a228da659e4866ec8716
erebrus <erebrus@gmail.com>
2024-11-24 17:09:13 +0000

    platform fixes

--- godot/src/world/platforms/platform.gd
+++ godot/src/world/platforms/platform.gd
@@ -1,6 +1,13 @@
+@tool
 extends StaticBody2D
 class_name Platform
 @export var floor_type:Types.FloorType
+@export var flipped:bool:
+	set(_v):
+		if _v != flipped:
+			$Sprite2D.flip_h=!$Sprite2D.flip_h
+			$CollisionPolygon2D.apply_scale(Vector2(-1,1))
+			
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
